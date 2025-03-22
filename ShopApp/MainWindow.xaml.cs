129082d56d438982475bf4cdb0eec2f72ed6d945
Vladimir Makarov <mordorsocial@disroot.org>
2025-03-22 22:01:54 +0500

    Даниил: Добавить прототип страницы аутентификации

--- ShopApp/MainWindow.xaml.cs
+++ ShopApp/MainWindow.xaml.cs
@@ -1,23 +1,23 @@
-﻿using Microsoft.EntityFrameworkCore;
-using ShopApp.Data;
+﻿using ShopApp.Database;
+using ShopApp.Pages;
 using System.Windows;
 
 namespace ShopApp;
 
-public partial class MainWindow : Window
-{
-    public MainWindow()
-    {
+public partial class MainWindow : Window {
+    public MainWindow() {
         InitializeComponent();
-        LoadUsers();
     }
-    private void LoadUsers()
-    {
-        using (var context = new AppDbContext())
-        {
-            var users = context.Users.ToList();
-            UsersDataGrid.ItemsSource = users;
-            UsersDataGrid.ItemsSource = users;          
-        }
+
+    private async void Window_Loaded(object sender, RoutedEventArgs e) {
+        MainFrame.Navigate(new AuthenticationPage());
+        //await LoadUsers();
     }
+
+    private async Task LoadUsers() {
+        var context = await AppDbContext.InitializeAsync();
+        var users = context.Users.ToList();
+        UsersDataGrid.ItemsSource = users;
+    }
+
 }
